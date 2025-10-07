82024f2437476a1299451dc54b8737414b715dcd
Matthew Higgins <mthiggins1s@semo.edu>
2025-10-07 14:27:33 -0500

    added consoel log for testing

--- server/server.js
+++ server/server.js
@@ -40,4 +40,5 @@ app.get('/protected', authMiddleware, (req, res) => {
 });
 
 const PORT = process.env.PORT || 3000;
-app.listen(PORT, () => console.log(`🚀 Server running on port ${PORT}`));
+console.log("🧠 About to start listening on port", PORT);
+app.listen(PORT, '0.0.0.0', () => console.log(`✅ Server running on port ${PORT}`));
